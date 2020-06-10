7009dd2b76de53d48658c7d2839036c9b71111de
johnnylarsonjr <daniel.del.fresno@gmail.com>
2020-06-10 20:43:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/redux/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <div className="App">
+          {list.map(function(item) {
+            return <div>{item.title}</div>;
+          })}
+        </div>
+      </header>
+    )
+  }
+}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h2>Let's make a React App MF!!!!</h2>
+//         <h3>Putting another text here</h3>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
