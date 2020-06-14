c66878af4b56ebd7b8fb5561b80ecc16f487dc54
johnnylarsonjr <daniel.del.fresno@gmail.com>
2020-06-14 21:35:27 +0100

    UPD

--- src/App.js
+++ src/App.js
@@ -2,84 +2,88 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/redux/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+// const list = [
+//   {
+//     title: 'React',
+//     url: 'https://facebook.github.io/react/',
+//     author: 'Jordan Walke',
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: 'Redux',
+//     url: 'https://github.com/redux/',
+//     author: 'Dan Abramov, Andrew Clark',
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   }
+// ]
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      list: list,
-    };
-    this.onDismiss = this.onDismiss.bind(this);
+//     this.state = {
+//       list: list,
+//     };
+//     this.onDismiss = this.onDismiss.bind(this);
+//   }
+//   onDismiss(id) {
+//     const isNotId = item => item.objectID !== id;
+//     const updateList = this.state.list.filter(isNotId);
+//     this.setState({ list: updateList });
+//   }
+//   render() {
+//     return (
+//       <header className="App-header">
+//         <div className="App">
+//           {this.state.list.map(item =>
+//             <div key={item.objectID}>
+//               <span>
+//                 <a href={item.url}>{item.title}</a>
+//               </span>
+//               <span>{item.author}</span>
+//               <span>{item.num_comments}</span>
+//               <span>{item.points}</span>
+//               <span>
+//                 <button onClick={() => this.onDismiss(item.objectID)} type="button">Dismiss</button>
+//               </span>
+//             </div>
+//           )}
+//         </div>
+//       </header>
+//     );
+//   }
+// }
+
+class ExplainBindingsComponent extends Component {
+  constructor() {
+    super();
+
+    this.onClickMe = () => {
+      console.log(this);
+    }
   }
-  onDismiss(id) {
-    const isNotId = item => item.objectID !== id;
-    const updateList = this.state.list.filter(isNotId);
-    this.setState({ list: updateList });
-  }
+
   render() {
     return (
       <header className="App-header">
-        <div className="App">
-          {this.state.list.map(item => 
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button onClick={() => this.onDismiss(item.objectID)} type="button">Dismiss</button>
-            </span>
-          </div>
-          )}
+        <div className="app">
+          <button
+            onClick={this.onClickMe}
+            type="button"
+            >
+            Click Me
+          </button>
         </div>
       </header>
     );
   }
 }
 
-class ExplainBindingsComponent extends Component {
-  constructor() {
-    super();
-    this.onClickMe = this.onClickMe.bind(this);
-  }
-
-  onClickMe () {
-    console.log(this);
-  }
-
-  render() {
-    return (
-      <button
-        onClick={this.onClickMe.bind(this)}
-        type="button">
-        Click Me    
-      </button>
-    );
-  }
-}
-
-new ExplainBindingsComponent();
+// new ExplainBindingsComponent();
 
 // class App extends Component {
 //   render() {
@@ -129,4 +133,5 @@ new ExplainBindingsComponent();
 //   );
 // }
 
-export default App;
+// export default App;
+export default ExplainBindingsComponent;
